d866c9a468108cf4c8c2b2a268c8b46856660780
s-haruki <e2023099@onomichi-u.ac.jp>
2024-05-31 01:23:59 +0900

    [Fix] 型つけ忘れ

--- src/db/history.ts
+++ src/db/history.ts
@@ -4,15 +4,9 @@ import SqliteDatabase from "./SqliteDatabase.ts";
 const db = SqliteDatabase();
 const listHistoryQuery = db.prepareQuery<
   [string, string, string, number, number, string],
+  History,
   {
-    payment_id: string;
-    paid_class: string;
-    timestamp: string;
-    total: number;
-    change: number;
-    product: string;
-  }, {
-    class_name: string
+    class_name: string;
   }
 >(`SELECT * FROM history WHERE paid_class = :class_name`);
 
@@ -38,9 +32,9 @@ export function addHistory(
     change,
     product,
   });
-  console.log("Add history: " + payment_id)
+  console.log("Add history: " + payment_id);
 }
 
 export function getAllHistories(class_name: string) {
-  return listHistoryQuery.allEntries({class_name})
+  return listHistoryQuery.allEntries({ class_name });
 }
--- src/db/users.ts
+++ src/db/users.ts
@@ -4,7 +4,10 @@ import SqliteDatabase from "./SqliteDatabase.ts";
 const db = SqliteDatabase();
 const query = db.prepareQuery<
   [string, string, string, string],
-  User
+  User,
+  {
+    email: string
+  }
 >(`SELECT * FROM users WHERE user_mail = :email`);
 
 export function getUserInfo(email: string) {
