7f05ddab763169f6020722b9a728a3bd98ac64d0
s-haruki <e2023099@onomichi-u.ac.jp>
2024-06-02 13:52:24 +0900

    [Add] addUser

--- src/db/users.ts
+++ src/db/users.ts
@@ -10,8 +10,20 @@ const query = db.prepareQuery<
   }
 >(`SELECT * FROM users WHERE user_mail = :email`);
 
+
+const addUserQuery = db.prepareQuery<
+  [],
+  Record<never, never>,
+  User
+>(`INSERT INTO users (user_mail, user_name, user_class, user_role) VALUES (:user_mail, :user_name, :user_class, :user_role)`);
+
 export function getUserInfo(email: string) {
   const q = query.firstEntry({ email });
   console.log(q)
   return q;
+}
+
+export function addUserInfo(param: User) {
+  addUserQuery.execute(param);
+  return param;
 }
--- src/user.ts
+++ src/user.ts
@@ -1,6 +1,8 @@
 import { Hono } from "hono";
 import type { JwtVariables } from "hono/jwt";
-import { getUserInfo } from "./db/users.ts";
+import { addUserInfo, getUserInfo } from "./db/users.ts";
+import { UserSchema } from "../schema.ts";
+import { zValidator } from "../util.ts";
 
 const app = new Hono<{ Variables: JwtVariables }>();
 
@@ -8,5 +10,12 @@ app.get("/", (c) => {
   const payload = c.get("jwtPayload");
   return c.json(getUserInfo(payload.email));
 });
+
+app.get("/add",
+  zValidator("json", UserSchema),
+  (c) => {
+    return c.json(addUserInfo(c.req.valid("json")));
+  }
+);
 
 export default app;
